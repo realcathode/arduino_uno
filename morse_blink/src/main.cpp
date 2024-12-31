50f51b5a1ca3f4f4a12e70148afed5a9283a469f
realcathode <ion.miron@tutanota.com>
2024-12-31 20:02:05 +0200

    Implement morse code blinking

--- morse_blink/src/main.cpp
+++ morse_blink/src/main.cpp
@@ -1,18 +1,69 @@
-#include <Arduino.h>
+#include "Arduino.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#define LED_BUILTIN 13
+#define DIT 100
+#define DAH 300
+#define MORSE_SPACE 150
+#define LETTER_SPACE 450
+#define WORD_SPACE 600
+
+const char *morse[] = {
+    ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---",
+    "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-",
+    "..-", "...-", ".--", "-..-", "-.--", "--.."
+};
+
+// Get the Morse code for a given letter
+const char *get_morse_code(char letter) {
+    if (letter >= 'a' && letter <= 'z') {
+        return morse[letter - 'a'];
+    }
+    return "";
+}
+
+// Blink a Morse code sequence
+void blink_morse_letter(const char *sequence) {
+    for (int i = 0; sequence[i] != '\0'; i++) {
+        if (sequence[i] == '.') {
+            digitalWrite(LED_BUILTIN, HIGH);
+            delay(DIT);
+            digitalWrite(LED_BUILTIN, LOW);
+        } else if (sequence[i] == '-') {
+            digitalWrite(LED_BUILTIN, HIGH);
+            delay(DAH);
+            digitalWrite(LED_BUILTIN, LOW);
+        }
+        if (sequence[i + 1] != '\0') {
+            delay(MORSE_SPACE);
+        }
+    }
+    delay(LETTER_SPACE);
+}
+
+// Blink a message in Morse code
+void blink_morse_message(const char *message) {
+    for (int i = 0; message[i] != '\0'; i++) {
+        char letter = message[i];
+        if (letter == ' ') {
+            delay(WORD_SPACE);
+            continue;
+        }
+        const char *morse_code = get_morse_code(letter);
+        blink_morse_letter(morse_code);
+    }
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+    const char *message = "hello morse";
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    Serial.print("Blinking message: ");
+    Serial.println(message);
+
+    blink_morse_message(message);
+    delay(5000);
 }
